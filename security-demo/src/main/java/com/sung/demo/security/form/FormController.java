b6aaecf242eaa8a5cc97938fb4b76a0c6fc677ee
Sung Park <castlewu9@gmail.com>
2021-02-08 23:20:37 +0900

    add spring-security demo

---
+++ security-demo/src/main/java/com/sung/demo/security/form/FormController.java
@@ -0,0 +1,39 @@
+package com.sung.demo.security.form;
+
+import java.security.Principal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FormController {
+
+  @GetMapping("/")
+  String index(Model model, Principal principal) {
+    if (principal == null) {
+      model.addAttribute("message", "Hello Spring Security");
+    } else {
+      model.addAttribute("message", "Hello " + principal.getName());
+    }
+    return "index";
+  }
+
+  @GetMapping("/info")
+  String info(Model model) {
+    model.addAttribute("message", "Hello Spring Security");
+    return "info";
+  }
+
+  @GetMapping("/dashboard")
+  String dashboard(Model model, Principal principal) {
+    model.addAttribute("message", "Hello " + principal.getName());
+    return "dashboard";
+  }
+
+  @GetMapping("/admin")
+  String admin(Model model, Principal principal) {
+    model.addAttribute("message", "Hello " + principal.getName());
+    return "admin";
+  }
+
+}
